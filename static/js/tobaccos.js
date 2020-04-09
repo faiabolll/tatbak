045f1e2caa9c2698329cbb0ef7a8847567c94278
faiabolll <egor.tarasov1999@gmail.com>
2020-04-09 15:16:52 +0300

    pc error merge

--- static/js/tobaccos.js
+++ static/js/tobaccos.js
@@ -1,8 +1,7 @@
 $(document).ready(function(){
 // # - means id
     $("#mix_create_submit").click(function(){
-        var serializedData = $("#mix_create_form").serialize();
-        
+        var serializedData = $("#mix_create_form").serialize();    
         $.ajax({
             url: $("#mix_create_form").data('url'),
             data: serializedData,
@@ -12,21 +11,13 @@ $(document).ready(function(){
                     '<div class="col s12 m12 l4"><div class="card small hoverable"><div class="card-image"><img src="{% static "images/pikcha.jpg" %}">' + 
                     '<span class="card-title"><b>' + response.new_mix.author_name + '</b>' + response.new_mix.structure + 
                     '</span></div><div class="card-content"><p>' + response.new_mix.description + 
-                    '</p></div><div class="card-action"><a href="#">This is a link</a>  </div></div></div>'
+                    '</p></div><div class="card-action"><a href="#">This is a link</a></div></div></div>'
                 )
             }
         });
     });
 
-    // $("#mix_create_button").click(function(){
-    //     $.ajax({
-    //         url: $("#mix_create_button").data('url'),
-    //         success: function(response){
-    //             $("#form_container").append(
-    //                 ''
-    //             )
-    //         }
-    //     });
-    // )};
+    $("#mix_create_button").click(function(){
+        $("#form_container").css('display', 'none')})
 
 });

ec189c03957d5955d00a6bdcc99e78d319662120
faiabolll <egor.tarasov1999@gmail.com>
2020-04-12 19:17:52 +0300

    fixed structure field in form

--- tobaccos/forms.py
+++ tobaccos/forms.py
@@ -4,16 +4,17 @@ from .models import Mix
 class MixForm(forms.ModelForm):
     class Meta:
         model = Mix
-        fields = "__all__"
+        fields = ['author_name', 'author_link', 'description', 'rating']
+        
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
-        self.fields['structure'].widget.attrs.update({
-            'id':"id_structure",
-            'placeholder':'first',
-            'secondaryPlaceholder':'second',
-        })
+        # self.fields['structure'].widget.attrs.update({
+        #     'id':"id_structure",
+        #     'placeholder':'first',
+        #     'secondaryPlaceholder':'second',
+        # })
         
         self.fields['author_link'].widget.attrs.update({
             'placeholder':'Introduce urself',
